f0329fc4de2b5a76b62299954779ca5aee9640fe
KelvinRdz <krodriguez50322@ufide.ac.cr>
2026-05-20 20:29:42 -0600

    commit 1
    1 Semana 1 prueba texto

---
+++ src/main/java/Tecshop/Kelvin_Rdz/KelvinRdzApplication.java
@@ -0,0 +1,13 @@
+package Tecshop.Kelvin_Rdz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KelvinRdzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KelvinRdzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tecshop/Kelvin_Rdz/KelvinRdzApplicationTests.java
@@ -0,0 +1,13 @@
+package Tecshop.Kelvin_Rdz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KelvinRdzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
